bbcec3f3ff2ed9abaeb8a40107ecefd5ce820a5c
Moriom <soya35-850@diu.edu.bd>
2023-11-30 18:18:49 +0600

    payment added

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const cors = require("cors");
 const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 // middleware
-const corsConfig = {
-  origin: ["http://localhost:5173"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-};
-app.use(cors(corsConfig));
+// const corsConfig = {
+//   origin: ["https://bistro-boss-634b2.web.app"],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+// };
+app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -28,11 +29,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const menuCollection = client.db("Rubicon").collection("menu");
     const cartCollection = client.db("Rubicon").collection("carts");
     const userCollection = client.db("Rubicon").collection("users");
+    const paymentCollection = client.db("Rubicon").collection("payments");
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -128,10 +130,42 @@ const verifyAdmin =async(req,res,next) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+app.patch('/menu/:id',async(req,res)=>{
+const item =req.body;
+const id =req.params.id;
+const filter ={_id:new ObjectId(id)}
+const updatedDoc ={
+  $set:{
+    name:item.name,
+    type:item.type,
+    status:item.status,
+    image:item.image
+  }
+}
+const result =await menuCollection.updateOne(filter,updatedDoc)
+res.send(result);
+})
+
+
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.post("/menu",verifyToken,verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+   
+    app.delete("/menu/:id",verifyToken,verifyAdmin,async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -154,8 +188,38 @@ const verifyAdmin =async(req,res,next) => {
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+// payment intent
+app.post("/create-payment-intent", async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount(items),
+      currency: "usd",
+   payment_method_types:['card'] 
+    });
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+})
+app.post('/payments', async(req,res) => {
+  const payment = req.body;
+  const paymentResult = await paymentCollection.insertOne(payment);
+
+  // carefully
+  console.log('payment info', payment);
+  const query = {_id:{
+    $in:payment.carIds.map(id=> new ObjectId(id) )
+  } };
+  const deleteResult = await cartCollection.deleteMany(query);
+  res.send({paymentResult,deleteResult});
+
+})
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
